8d5d9d2e3e5c4dec23facd5fed7dac46015ab982
Zdravko Šplajt <zdravek.sprite@gmail.com>
2021-12-02 20:25:57 +0100

    Socialite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,5 +52,27 @@
         </x-button>
       </div>
     </form>
+
+    <div class="flex justify-between items-center mt-3">
+      <hr class="w-full"> <span class="p-2 text-gray-400 mb-1">OR</span>
+      <hr class="w-full">
+    </div>
+    <div class="flex items-center justify-end mt-4">
+      <a href="login/facebook" class="inline-flex items-center px-4 py-2 space-x-1 border border-transparent rounded-md text-sm text-gray-600 hover:text-gray-900">
+        <svg class="centerHV" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 267 267" width="25" height="25">
+          <g fill="white">
+            <path id="Blue_1_" fill="#3C5A99" d="M248.082,262.307c7.854,0,14.223-6.369,14.223-14.225V18.812
+	c0-7.857-6.368-14.224-14.223-14.224H18.812c-7.857,0-14.224,6.367-14.224,14.224v229.27c0,7.855,6.366,14.225,14.224,14.225
+	H248.082z" />
+            <path id="f" fill="#FFFFFF" d="M182.409,262.307v-99.803h33.499l5.016-38.895h-38.515V98.777c0-11.261,3.127-18.935,19.275-18.935
+	l20.596-0.009V45.045c-3.562-0.474-15.788-1.533-30.012-1.533c-29.695,0-50.025,18.126-50.025,51.413v28.684h-33.585v38.895h33.585
+	v99.803H182.409z" />
+          </g>
+        </svg>
+        <strong>{{ __('Login') }}</strong>
+        <span>{{ __('with') }}</span>
+        <strong>Facebook</strong>
+      </a>
+
   </x-auth-card>
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\Admin\ImpersonateController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redirect;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -63,3 +66,42 @@ Route::prefix('admin')->middleware(['auth.admin'])->group(function () {
     ]);
   Route::get('/impersonate/{id}', [ImpersonateController::class, 'start'])->name('impersonate.start');
 });
+
+Route::get('login/{provider}', function ($provider) {
+  return Socialite::driver($provider)->redirect();
+})->name('{provider}Login');
+Route::get('login/{provider}/callback', function ($provider) {
+  $social_user = Socialite::driver($provider)->user();
+  // $user->token
+  $user = User::firstOrCreate([
+    'email' => $social_user->getEmail(),
+  ]);
+  if (!$user->name) {
+    $user->name = $social_user->getName();
+  }
+  if (!$user[$provider . "_id"]) {
+    $user[$provider . "_id"] = $social_user->getId();
+  }
+  if ($social_user->getAvatar()) {
+    $url = $social_user->getAvatar() . '&access_token=' . $social_user->token;
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
+    $res = curl_exec($ch);
+    $redirectedUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+    //$avatar = ltrim($redirectedUrl,"https://");
+    $avatar = $redirectedUrl;
+    if (!$user->avatar) {
+      $user->avatar = $avatar;
+    }
+    if (!$user[$provider . "_avatar"]) {
+      $user[$provider . "_avatar"] = $avatar;
+    }
+  }
+  $user->save();
+  Auth::Login($user, true);
+  return redirect(route('home'));
+})->name('{provider}Callback');
