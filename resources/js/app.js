87f84c64610c625ee65f7c8ecdc158530f7097d8
Zdravko Šplajt <zdravek.sprite@gmail.com>
2021-12-05 18:07:05 +0100

    Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,13 @@
 require('./bootstrap');
 
+import Vue from 'vue';
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+const app = new Vue({
+  el: '#app',
+});
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
